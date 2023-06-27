42e0cc6d84e811e647301834c2f569f1bde7b7ac
Illia Shyian <shyian_id@groupbwt.com>
2023-06-27 20:08:53 +0300

    feat: added migrations for Company and Price

---
+++ database/migrations/2023_06_27_165342_create_prices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('company_id')->unsigned();
+            $table->integer('quantity')->unsigned();
+            $table->float('cost')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('prices', function (Blueprint $table) {
+            $table->dropForeign('companies_ccompany_id_foreign');
+        });
+        Schema::dropIfExists('prices');
+    }
+};
