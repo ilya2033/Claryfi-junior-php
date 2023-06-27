5e76e2a73bc8de7d687ef8245bd25b3578460b26
Illia Shyian <shyian_id@groupbwt.com>
2023-06-27 18:30:06 +0300

    feat: install vue3 + vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+import App from "./layouts/App.vue";
+
+import { createApp } from "vue";
+import { vuetify } from "./plugins/vuetify";
+const app = createApp(App, { vuetify });
+
+app.use(vuetify);
+app.mount("#app");
