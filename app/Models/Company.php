8958d4230da0832a9a9d905ae33f247b82315995
Illia Shyian <shyian_id@groupbwt.com>
2023-06-27 20:18:37 +0300

    feat: added Company and Price models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Company extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $casts = [
+        'name' => 'string',
+    ];
+
+    public function prices(): BelongsToMany
+    {
+        return $this->belongsToMany(Price::class);
+    }
 }
--- app/Models/Price.php
+++ app/Models/Price.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Price extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'cost',
+        'quantity'
+    ];
+
+    protected $casts = [
+        'company_id' => 'int',
+        'cost' => 'float',
+        'quantity' => 'integer',
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
